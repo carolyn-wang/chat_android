2b244d26a71119ceb20d0b17a330c09445a4de53
Carolyn Wang <carolynw@fb.com>
2022-06-13 11:05:28 -0700

    finished step 3

--- app/src/main/java/com/example/simplechat/SimpleChat.java
+++ app/src/main/java/com/example/simplechat/SimpleChat.java
@@ -3,12 +3,49 @@ package com.example.simplechat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.LogInCallback;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class SimpleChat extends AppCompatActivity {
+
+    final String TAG = "SimpleChat";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ParseUser.getCurrentUser() != null) { // Start with existing user
+            startWithCurrentUser(); //TODO: We will build out this method in the next step
+        } else { // If not logged in, login as a new anonymous user
+            login();
+        }
+
     }
+
+    // Create an anonymous user using ParseAnonymousUtils and set sUserId
+    void login() {
+        ParseAnonymousUtils.logIn(new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Anonymous login failed: ", e);
+                } else {
+                    startWithCurrentUser();
+                }
+            }
+        });
+    }
+
+
+
+    public void setupMessagePosting(){
+
+    }
+
+
 }
