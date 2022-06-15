c008cfcedcbb830eb18714e3d7faa69870cb55ea
Carolyn Wang <carolynw@fb.com>
2022-06-15 10:23:02 -0700

    fixed bug, messages showing up

--- app/src/main/java/com/example/simplechat/ChatActivity.java
+++ app/src/main/java/com/example/simplechat/ChatActivity.java
@@ -1,24 +1,43 @@
 package com.example.simplechat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.LogInCallback;
 import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
-import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+import com.parse.livequery.ParseLiveQueryClient;
+import com.parse.livequery.SubscriptionHandling;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 public class ChatActivity extends AppCompatActivity {
     static final String TAG = ChatActivity.class.getSimpleName();
+    static final int MAX_CHAT_MESSAGES_TO_SHOW = 50;
+    private ImageButton ibSend;
+    private EditText etMessage;
+    private ArrayList mMessages;
+    private RecyclerView rvChat;
+    private boolean mFirstLoad;
+    private ChatAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +49,41 @@ public class ChatActivity extends AppCompatActivity {
         } else { // If not logged in, login as a new anonymous user
             login();
         }
-    }
 
-    // Get the userId from the cached currentUser object
-    void startWithCurrentUser() {
-        // TODO:
+        // Load existing messages to begin with
+        refreshMessages();
+
+        // Make sure the Parse server is setup to configured for live queries
+        // Enter the websocket URL of your Parse server
+        String websocketUrl = "wss://codepathparsechatlab.b4a.io/"; // ⚠️ TYPE IN A VALID WSS:// URL HERE
+
+        ParseLiveQueryClient parseLiveQueryClient = null;
+        try {
+            parseLiveQueryClient = ParseLiveQueryClient.Factory.getClient(new URI(websocketUrl));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        ParseQuery<Message> parseQuery = ParseQuery.getQuery(Message.class);
+        // This query can even be more granular (i.e. only refresh if the entry was added by some other user)
+        // parseQuery.whereNotEqualTo(USER_ID_KEY, ParseUser.getCurrentUser().getObjectId());
+
+        // Connect to Parse server
+        SubscriptionHandling<Message> subscriptionHandling = parseLiveQueryClient.subscribe(parseQuery);
+
+        // Listen for CREATE events on the Message class
+        subscriptionHandling.handleEvent(SubscriptionHandling.Event.CREATE, (query, object) -> {
+            mMessages.add(0, object);
+
+            // RecyclerView updates need to be run on the UI thread
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mAdapter.notifyDataSetChanged();
+                    rvChat.scrollToPosition(0);
+                }
+            });
+        });
     }
 
     // Create an anonymous user using ParseAnonymousUtils and set sUserId
@@ -50,90 +99,106 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
     }
+
+    // Get the userId from the cached currentUser object
+    void startWithCurrentUser() {
+        setupMessagePosting();
+    }
+
+    // Get the items in the reverse order:
+    void setupMessagePosting() {
+        etMessage = (EditText) findViewById(R.id.etMessage);
+        ibSend = (ImageButton) findViewById(R.id.ibSend);
+        rvChat = (RecyclerView) findViewById(R.id.rvChat);
+        mMessages = new ArrayList<>();
+        mFirstLoad = true;
+        final String userId = ParseUser.getCurrentUser().getObjectId();
+        mAdapter = new ChatAdapter(ChatActivity.this, userId, mMessages);
+        rvChat.setAdapter(mAdapter);
+
+        // associate the LayoutManager with the RecylcerView
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ChatActivity.this);
+        linearLayoutManager.setReverseLayout(true);
+        rvChat.setLayoutManager(linearLayoutManager);
+
+        ibSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = etMessage.getText().toString();
+                //ParseObject message = ParseObject.create("Message");
+                //message.put(Message.USER_ID_KEY, userId);
+                //message.put(Message.BODY_KEY, data);
+                // Using new `Message` Parse-backed model now
+                Message message = new Message();
+                message.setUserId(ParseUser.getCurrentUser().getObjectId());
+                message.setBody(data);
+                Log.i("Message", "save message");
+                message.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        Log.i("Message", "created message");
+                        Toast.makeText(ChatActivity.this, "Successfully created message on Parse",
+                                Toast.LENGTH_SHORT).show();
+                        refreshMessages();
+                    }
+                });
+                etMessage.setText(null);
+            }
+        });
+    }
+
+    // Query messages from Parse so we can load them into the chat adapter
+    void refreshMessages() {
+        // Construct query to execute
+        ParseQuery<Message> query = ParseQuery.getQuery(Message.class);
+        // Configure limit and sort order
+        query.setLimit(MAX_CHAT_MESSAGES_TO_SHOW);
+
+        // get the latest 50 messages, order will show up newest to oldest of this group
+        query.orderByDescending("createdAt");
+        // Execute query to fetch all messages from Parse asynchronously
+        // This is equivalent to a SELECT query with SQL
+        query.findInBackground(new FindCallback<Message>() {
+            public void done(List<Message> messages, ParseException e) {
+                if (e == null) {
+                    mMessages.clear();
+                    mMessages.addAll(messages);
+                    mAdapter.notifyDataSetChanged(); // update adapter
+                    // Scroll to the bottom of the list on initial load
+                    if (mFirstLoad) {
+                        rvChat.scrollToPosition(0);
+                        mFirstLoad = false;
+                    }
+                } else {
+                    Log.e("message", "Error Loading Messages" + e);
+                }
+            }
+        });
+    }
+
+    // Create a handler which can run code periodically
+    static final long POLL_INTERVAL = TimeUnit.SECONDS.toMillis(3);
+    Handler myHandler = new android.os.Handler();
+    Runnable mRefreshMessagesRunnable = new Runnable() {
+        @Override
+        public void run() {
+            refreshMessages();
+            myHandler.postDelayed(this, POLL_INTERVAL);
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Only start checking for new messages when the app becomes active in foreground
+//        myHandler.postDelayed(mRefreshMessagesRunnable, POLL_INTERVAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // Stop background task from refreshing messages, to avoid unnecessary traffic & battery drain
+        myHandler.removeCallbacksAndMessages(null);
+        super.onPause();
+    }
 }
-
-
-
-
-//public class ChatActivity extends AppCompatActivity implements View.OnClickListener {
-//
-//    private final String TAG = "ChatActivity";
-//    private ImageButton ibSend;
-//    private EditText etMessage;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_chat);
-//
-//        if (ParseUser.getCurrentUser() != null) { // Start with existing user
-//            startWithCurrentUser(); //TODO: We will build out this method in the next step
-//        } else { // If not logged in, login as a new anonymous user
-//            login();
-//        }
-//
-//    }
-//
-//    // Create an anonymous user using ParseAnonymousUtils and set sUserId
-//    void login() {
-//        ParseAnonymousUtils.logIn(new LogInCallback() {
-//            @Override
-//            public void done(ParseUser user, ParseException e) {
-//                if (e != null) {
-//                    Log.e(TAG, "Anonymous login failed: ", e);
-//                } else {
-//                    startWithCurrentUser();
-//                }
-//            }
-//        });
-//    }
-//
-//
-//
-//    public void setupMessagePosting() {
-//        ibSend = (ImageButton) findViewById(R.id.ibSend);
-//        etMessage = (EditText) findViewById(R.id.etMessage);
-//        ibSend.setOnClickListener(this);
-//    }
-//
-//
-//    @Override
-//    public void onClick(View v) {
-////        Get the message String
-//        String message = etMessage.getText().toString();
-////        Create a ParseObject for the message and save the USER_ID_KEY and BODY_KEY values in it.
-//
-//        ParseObject parseMessage = new ParseObject("parseMessage");
-////        parseMessage.put("USER_ID_KEY", 1232);
-////        parseMessage.put("BODY_KEY", 1232 );
-//
-////        Call the saveInBackground() method to persist the data to the database and pass it a call to SaveCallback().
-////        You can choose to toast a message on save.
-//        parseMessage.saveInBackground(new SaveCallback() {
-//            @Override
-//            public void done(ParseException e) {
-//                if (e == null) {
-//                    Toast.makeText(ChatActivity.this, "Successfully created message on Parse",
-//                            Toast.LENGTH_SHORT).show();
-//                } else {
-//                    Log.e(TAG, "Failed to save message", e);
-//                }
-//            }
-//        });
-//
-//        Toast.makeText(this, "save text", Toast.LENGTH_SHORT).show();
-//
-////        Clear the EditText component
-//        etMessage.clearComposingText();
-//
-//    }
-//
-//    @Override
-//    public void onPointerCaptureChanged(boolean hasCapture) {
-//        super.onPointerCaptureChanged(hasCapture);
-//    }
-//
-//    public void startWithCurrentUser(){
-//        setupMessagePosting();
-//    }
-//}
--- app/src/main/java/com/example/simplechat/ChatApplication.java
+++ app/src/main/java/com/example/simplechat/ChatApplication.java
@@ -3,8 +3,15 @@ package com.example.simplechat;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.livequery.ParseLiveQueryClient;
+import com.parse.livequery.SubscriptionHandling;
+
+import java.net.URISyntaxException;
 
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 public class ChatApplication extends Application {
     private final String applicationId = "BgMeRhXN4Mfsr4iu7cg2PJPITPJgn9T4meopyWPG";
@@ -14,12 +21,20 @@ public class ChatApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        ParseObject.registerSubclass(Message.class);
+        // Use for monitoring Parse network traffic
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
-// ...
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(applicationId) // provided in Lab instructions
                 .clientKey(clientKey) // provided in Lab instructions
                 .clientBuilder(builder)
                 .server(server).build());  // provided in Lab instructions
+
     }
 }
